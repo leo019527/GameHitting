9ad5e0a538b40a44a5cb4edf5424f03dd318ceb3
leo019527 <15281070@bjtu.edu.cn>
2017-07-16 22:25:36 +0800

    暂时完成了蓝牙连接（因为设备原因没有测试过）

--- app/src/main/java/com/example/dong/gamehit/DoubleActivity.java
+++ app/src/main/java/com/example/dong/gamehit/DoubleActivity.java
@@ -1,20 +1,22 @@
 package com.example.dong.gamehit;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class DoubleActivity extends AppCompatActivity {
 
-    private boolean isServer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.double_modle);
-        Intent intent = getIntent();
-        if(intent != null)
-        {
-            isServer = intent.getBooleanExtra("choose",true);
-        }
     }
 }
--- app/src/main/java/com/example/dong/gamehit/MainActivity.java
+++ app/src/main/java/com/example/dong/gamehit/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.dong.gamehit;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private static boolean isServer;
     private static Intent intent = null;
     int halt;
+    private static MainActivity a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        a = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         halt = 0;
@@ -88,8 +99,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //蓝牙相关变量
+    public static final UUID uuid= UUID.fromString("245dc87c-683d-11e7-907b-a6006ad3dba0");
+    private static boolean isOver=false;
+    private BluetoothAdapter mAdapter=null;
+    private Set<BluetoothDevice> blueSet = new HashSet<>();
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //找到设备
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+
+                    blueSet.add(device);
+                }
+            }
+            //搜索完成
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED
+                    .equals(action)) {
+                MainActivity.isOver=true;
+            }
+        }
+    };
+    private static BluetoothSocket clienSocket;
+    private static BluetoothServerSocket serverSocketTmp;
+    private static BluetoothSocket serverSocket;
 
     public void doubleClisk(View view) {
+        //判断蓝牙可用
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mAdapter == null)
+        {
+            Toast.makeText(this,"本地蓝牙不可用", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //打开蓝牙模块
+        if(!mAdapter.isEnabled()){
+            //弹出对话框提示用户是后打开
+            Intent enabler = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enabler, 0);
+        }
+
         //选择是否服务端
         android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this,android.R.style.Theme_Holo_Light_Dialog);
         builder.setTitle("选择阵营");
@@ -100,16 +153,123 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             {
                 if (which == 1)
                 {
-                    intent.putExtra("choose",true);
-                    startActivity(intent);
+                    //服务器端
+                    //设置蓝牙可见
+                    if (mAdapter.isEnabled()) {
+                        if (mAdapter.getScanMode() !=
+                                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+                            Intent discoverableIntent = new Intent(
+                                    BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                            discoverableIntent.putExtra(
+                                    BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                            startActivity(discoverableIntent);
+                        }
+                    }
+                    //服务器端开始监听
+                    new AcceptThread("doublehit",uuid,serverSocketTmp).run();
                 }
                 else
                 {
-                    intent.putExtra("choose",false);
-                    startActivity(intent);
+                    //客户端
+                    //添加蓝牙查找
+                    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                    registerReceiver(mReceiver, filter);
+                    filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+                    registerReceiver(mReceiver, filter);
+                    // 如果正在搜索，就先取消搜索
+                    if (mAdapter.isDiscovering()) {
+                        mAdapter.cancelDiscovery();
+                    }
+                    // 开始搜索蓝牙设备,搜索到的蓝牙设备通过广播返回
+                    mAdapter.startDiscovery();
+                    new ConnectThread(a).run();
+
                 }
             }
         });
         builder.show();
+    }
+    //服务器端连接线程
+    private class AcceptThread extends Thread
+    {
+        private String name;
+        private UUID uuid;
+        private BluetoothServerSocket serverSocket;
+        private BluetoothSocket socket;
+
+        public AcceptThread(String name, UUID uuid, BluetoothServerSocket serverSocket) {
+            this.name = name;
+            this.uuid = uuid;
+            this.serverSocket = serverSocket;
+        }
+
+        @Override
+        public void run() {
+            //服务器端开始监听
+            try {
+                serverSocket = mAdapter. listenUsingRfcommWithServiceRecord("hit",uuid);
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //TODO:传消息
+            startActivity(intent);
+        }
+    }
+
+    //客户端连接线程
+    private class ConnectThread extends Thread{
+        MainActivity a;
+
+        public ConnectThread(MainActivity a) {
+            this.a = a;
+        }
+
+        String choosenOne;
+        @Override
+        public void run() {
+            while(!MainActivity.isOver)
+            {
+                System.out.println(blueSet.size());
+            }
+            //显示选择界面
+            android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(a,android.R.style.Theme_Holo_Light_Dialog);
+            //builder.setIcon(R.drawable.ic_launcher);
+            builder.setTitle("选择一个蓝牙进行连接");
+            //    指定下拉列表的显示数据
+            final String[] blueTooth= new String[blueSet.size()];
+            final Iterator<BluetoothDevice> iterator = blueSet.iterator();
+            for(int i=0;i<blueSet.size();i++)
+            {
+                blueTooth[i] = iterator.next().getName();
+            }
+            //    设置一个下拉的列表选择项
+            builder.setItems(blueTooth, new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    choosenOne = blueTooth[which];
+                    Iterator<BluetoothDevice> iterator1 = blueSet.iterator();
+                    BluetoothDevice a;
+                    while(iterator.hasNext())
+                    {
+                        a = iterator.next();
+                        if (a.getName()==choosenOne)
+                        {
+                            try {
+                                MainActivity.clienSocket = a.createRfcommSocketToServiceRecord(uuid);
+                                clienSocket.connect();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            //TODO:传数据
+                            startActivity(intent);
+                        }
+                    }
+                }
+            });
+            builder.show();
+        }
     }
 }
